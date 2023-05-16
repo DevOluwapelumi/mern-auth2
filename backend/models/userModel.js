abba7f0be6571770c33772b10931109bc4408f94
Olaniyi Sunday Emmanuel <Olaniyisunday323@gmail.com>
2023-05-16 12:27:23 -0700

    User login and logout

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,10 @@ userShema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, salt)
 })
 
+userShema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
+
 const User = mongoose.model('users', userShema)
 
 export default User
